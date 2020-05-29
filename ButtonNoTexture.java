e2273ecf72ec455874ce24693573094149422f10
Linkheroes <41187332+Linkheroes@users.noreply.github.com>
2020-05-29 21:07:31 +0200

    Add files via upload

---
+++ ButtonNoTexture.java
@@ -0,0 +1,48 @@
+fr.tonpackage;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Resource;
+
+public class ButtonNoTexture extends GuiButton {
+
+    private ResourceLocation noTexture = new ResourceLocation("textures/gui/ButtonNoTexture.png");
+
+    public ButtonNoTexture(int buttonId, int x, int y, int widthIn, int heightIn, String buttonText) {
+        super(buttonId, x, y, widthIn, heightIn, buttonText);
+    }
+
+    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        if (this.visible)
+        {
+            FontRenderer fontrenderer = mc.fontRendererObj;
+            mc.getTextureManager().bindTexture(noTexture);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            this.hovered = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+            int i = this.getHoverState(this.hovered);
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+            GlStateManager.blendFunc(770, 771);
+            this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, 46 + i * 20, this.width / 2, this.height);
+            this.drawTexturedModalRect(this.xPosition + this.width / 2, this.yPosition, 200 - this.width / 2, 46 + i * 20, this.width / 2, this.height);
+            this.mouseDragged(mc, mouseX, mouseY);
+            int j = 14737632;
+
+            if (!this.enabled)
+            {
+                j = 10526880;
+            }
+            else if (this.hovered)
+            {
+                j = 16777120;
+            }
+
+            this.drawCenteredString(fontrenderer, this.displayString, this.xPosition + this.width / 2, this.yPosition + (this.height - 8) / 2, j);
+        }
+    }
+
+}
